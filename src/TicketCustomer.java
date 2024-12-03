766c899afa70aefec58da02b0f786dc213a3bf32
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-03 12:24:01 +0530

    Added TicketSystem

--- src/TicketCustomer.java
+++ src/TicketCustomer.java
@@ -1,2 +1,25 @@
-public class TicketCustomer {
+public class TicketCustomer implements Runnable{
+    private final TicketPool ticketPool;
+    private final int retrievalRate;
+
+    public TicketCustomer(TicketPool ticketPool, int retrievalRate){
+        this.ticketPool=ticketPool;
+        this.retrievalRate=retrievalRate;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while(!Thread.currentThread().isInterrupted()){
+                String ticket=ticketPool.removeTicket();
+                if (ticket!=null){
+                    System.out.println("Customer purchased: " + ticket);
+                }
+                Thread.sleep(1000/retrievalRate);
+            }
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            System.err.println("Customer interrupted.");
+        }
+    }
 }
--- src/TicketVendor.java
+++ src/TicketVendor.java
@@ -1,2 +1,24 @@
-public class TicketVendor {
+public class TicketVendor implements Runnable{
+    private final TicketPool ticketPool;
+    private final int ticketsAvailable;
+    private final int releaseRate;
+
+    public TicketVendor(TicketPool ticketPool, int ticketsToGenerate, int releaseRate){
+        this.ticketPool=ticketPool;
+        this.ticketsAvailable=ticketsToGenerate;
+    }
+
+    @Override
+    public void run(){
+        try{
+            for(int i=1, i<=ticketsAvailable; i++){
+                String ticket = "Ticket-" +i;
+                ticketPool.addTicket(ticket);
+                Thread.sleep(1000/releaseRate);
+            }
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            System.err.println("Vendor interrupted.");
+        }
+    }
 }
