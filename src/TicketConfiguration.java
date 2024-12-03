7bfb4e83685c94ba94bbad7439e7976e1a6dd916
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-03 11:51:43 +0530

    Added Configuration

---
+++ src/TicketConfiguration.java
@@ -0,0 +1,47 @@
+public class TicketConfiguration {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+
+    public TicketConfiguration(){}
+
+    public TicketConfiguration(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketCapacity){
+        this.totalTickets=totalTickets;
+        this.ticketReleaseRate=ticketReleaseRate;
+        this.customerRetrievalRate=customerRetrievalRate;
+        this.maxTicketCapacity=maxTicketCapacity;
+    }
+
+    public int getTotalTickets(){
+        return totalTickets;
+    }
+
+    public int getTicketReleaseRate() {
+        return ticketReleaseRate;
+    }
+
+    public int getCustomerRetrievalRate() {
+        return customerRetrievalRate;
+    }
+
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
+    }
+
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+
+    public void setTicketReleaseRate(int ticketReleaseRate) {
+        this.ticketReleaseRate = ticketReleaseRate;
+    }
+
+    public void setCustomerRetrievalRate(int customerRetrievalRate) {
+        this.customerRetrievalRate = customerRetrievalRate;
+    }
+
+    public void setMaxTicketCapacity(int maxTicketCapacity) {
+        this.maxTicketCapacity = maxTicketCapacity;
+    }
+}
