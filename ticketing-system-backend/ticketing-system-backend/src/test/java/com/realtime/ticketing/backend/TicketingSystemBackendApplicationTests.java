1be7a799228b01a9b85048729d27fb3f7d3f4edd
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-06 10:34:06 +0530

    Real-Time Ticket System Backend

---
+++ ticketing-system-backend/ticketing-system-backend/src/test/java/com/realtime/ticketing/backend/TicketingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtime.ticketing.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
