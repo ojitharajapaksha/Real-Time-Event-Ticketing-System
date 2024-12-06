5577b8b32b85a7ccafd432b3d7d22c98988f02f9
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-06 08:42:12 +0530

    Real_Time Ticket System Backend

---
+++ src/main/java/com/realtime/ticketing/backend/TicketingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.realtime.ticketing.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemBackendApplication.class, args);
+	}
+
+}
