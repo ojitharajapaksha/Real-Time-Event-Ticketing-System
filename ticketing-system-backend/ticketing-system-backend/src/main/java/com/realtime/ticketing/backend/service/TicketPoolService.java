9599c407344086161254a52aa67ed010b06ecc72
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-06 13:32:43 +0530

    Real-Time Ticket System Backend

---
+++ ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/backend/service/TicketPoolService.java
@@ -0,0 +1,27 @@
+package com.realtime.ticketing.backend.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.BlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+@Service
+public class TicketPoolService {
+    private final BlockingQueue<String> tickets = new LinkedBlockingQueue<>();
+    private final int maxCapacity = 10;
+
+    public void addTicket(String ticket) throws InterruptedException{
+        if (ticket.size() >= maxCapacity){
+            Thread.sleep(100);
+        }
+        ticket.put(ticket);
+    }
+    public String removeTicket(){
+        return tickets.poll();
+    }
+    public int getTicketCount{
+        return tickets.size();
+    }
+}
+
