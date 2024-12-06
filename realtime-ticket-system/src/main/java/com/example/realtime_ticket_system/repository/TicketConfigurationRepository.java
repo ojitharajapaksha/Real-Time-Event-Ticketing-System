de864b1e7cac433f45fd23d0094dbd00a773cd32
Ojitha Rajapaksha <162859533+ojitharajapaksha@users.noreply.github.com>
2024-12-06 08:18:14 +0530

    Delete realtime-ticket-system directory

--- realtime-ticket-system/src/main/java/com/example/realtime_ticket_system/repository/TicketConfigurationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.realtime_ticket_system.repository;
-
-import com.example.realtime_ticket_system.model.TicketConfiguration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketConfigurationRepository extends JpaRepository<TicketConfiguration, Long> {
-}
