c1b9bb82e28ca0a6518e6a1304759bf14ab311db
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-11 13:16:13 +0530

    Real-Time Ticket System Backend

--- ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/threads/TicketCustomer.java
+++ ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/threads/TicketCustomer.java
@@ -1,4 +1,41 @@
 package com.realtime.ticketing.system.backend.threads;
 
-public class TicketCustomer {
+import com.realtime.ticketing.system.backend.service.TicketService;
+import java.util.concurrent.TimeUnit;
+
+public class TicketCustomer implements  Runnable{
+
+    private final TicketService ticketService;
+    private final int retrievalRate;
+
+    public TicketCustomer(TicketService ticketService, int retrievalRate){
+        this.ticketService = ticketService;
+        this.retrievalRate = retrievalRate;
+    }
+
+    @Override
+    public void run(){
+        while (!Thread.currentThread().isInterrupted()){
+            String ticket = ticketService.removeTicket();
+            if(ticket != null){
+                System.out.println("Ticket puchased: " + ticket);
+            }else {
+                System.out.println("No tickets available for purchase.");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                }catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+
+            try{
+                TimeUnit.MILLISECONDS.sleep(1000/retrievalRate);
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+        System.out.println("TicketCustomer thread has been stopped.");
+    }
 }
