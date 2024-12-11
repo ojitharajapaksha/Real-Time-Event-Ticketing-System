310ee9e1ff3839a1dd2a6c5252ee3b64a548d647
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-11 22:27:32 +0530

    Real-Time Ticketing System Backend

--- Real-Time-Ticket-System-Backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/config/WebConfig.java
+++ Real-Time-Ticket-System-Backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/config/WebConfig.java
@@ -1,3 +1,4 @@
+// Enabling CORS in the backend
 package com.realtime.ticketing.system.backend.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Configuring CORS mapping
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        // Allow CORS for all api endpoints
         registry.addMapping("/api/**")
+                // Allow request from the React frontend
                 .allowedOrigins("http://localhost:3000")
+                // Specify the HTTP methods
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
+                // Allow cookies and other credentials
                 .allowCredentials(true);
     }
 }
