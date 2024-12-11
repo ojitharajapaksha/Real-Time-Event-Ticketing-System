6c83c30da3069f25abce0c3c158cd73a842b2618
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-11 13:21:24 +0530

    Real-Time Ticket System Backend

--- ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/threads/TicketVendor.java
+++ ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/threads/TicketVendor.java
@@ -1,4 +1,24 @@
 package com.realtime.ticketing.system.backend.threads;
 
-public class TicketVendor {
+import com.realtime.ticketing.system.backend.service.TicketService
+
+public class TicketVendor implements Runnable{
+    private final TicketService ticketService;
+    private final int ticketsToGenarate;
+    private final int releaseRate;
+
+    public TicketVendor(TicketService ticketService, int ticketsToGenarate. int releaseRate){
+        this.ticketService=ticketService;
+        this.ticketsToGenarate=ticketsToGenarate;
+        this.releaseRate=releaseRate;
+    }
+
+    @Override
+    public void run(){
+        for(int i=1; i<=ticketsToGenarate; i++){
+            String ticket = "Ticket" + i;
+            ticketService.addTicket(ticket);
+            System.out.println("Ticket added: " + ticket);
+        }
+    }
 }
