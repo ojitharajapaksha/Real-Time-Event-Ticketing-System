d739daf93ac204bdeda8bfe5343a70d43e69eab5
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-11 12:45:36 +0530

    Real-Time Ticket System Backend

--- ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/model/TicketLog.java
+++ ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/model/TicketLog.java
@@ -1,4 +1,29 @@
 package com.realtime.ticketing.system.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class TicketLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+    private String type;
+    private String action;
+
+    public TicketLog() {
+    }
+
+    public TicketLog(String message, Date timestamp, String type, String action) {
+        this.message = message;
+        this.timestamp = timestamp;
+        this.type = type;
+        this.action = action;
+    }
 }
--- ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/model/TicketPool.java
+++ ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/model/TicketPool.java
@@ -1,4 +1,13 @@
 package com.realtime.ticketing.system.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class TicketPool {
+
+    @Id
+    @GeneratedValue(strategy = )
 }
