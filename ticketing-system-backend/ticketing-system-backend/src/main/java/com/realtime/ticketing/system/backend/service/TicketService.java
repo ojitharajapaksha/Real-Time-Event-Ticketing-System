3df793f4fff32f32bbfbd612350fe024d2f456c3
Ojitha Rajapaksha <ojitharajapaksha@gmail.com>
2024-12-11 13:07:26 +0530

    Real-Time Ticket System Backend

--- ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/service/TicketService.java
+++ ticketing-system-backend/ticketing-system-backend/src/main/java/com/realtime/ticketing/system/backend/service/TicketService.java
@@ -1,26 +1,49 @@
 package com.realtime.ticketing.system.backend.service;
 
+import com.realtime.ticketing.system.backend.model.TicketConfiguration;
+import com.realtime.ticketing.system.backend.model.TicketLog;
+import com.realtime.ticketing.system.backend.model.TicketPool;
+import com.realtime.ticketing.system.backend.repository.TicketLogRepository;
+import com.realtime.ticketing.system.backend.repository.TicketPoolRepository;
+import com.realtime.ticketing.system.backend.threads.TicketCustomer;
+import com.realtime.ticketing.system.backend.threads.TicketVendor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.*;
+import java.util.concurrent.*;
 
 @Service
 public class TicketService {
-    private final BlockingQueue<String> tickets = new LinkedBlockingQueue<>();
-    private final int maxCapacity = 10;
 
-    public void addTicket(String ticket) throws InterruptedException{
-        if (tickets.size() >= maxCapacity){
-            Thread.sleep(100);
+    @Autowired
+    private TicketLogRepository logRepository;
+
+    @Autowired
+    private TicketPoolRepository ticketPoolRepository;
+
+    private TicketConfiguration configuration;
+    private final BlockingQueue<String> ticketQueue;
+    private final ExecutorService = new LinkedBlockingQueue<>();
+    private boolean isRunning;
+
+    public TicketService(){
+        this.isRunning = false;
+        this.ticketQueue = new LinkedBlockingQueue<>();
+        this.executorService = Executors.newCachedThreadPool();
+    }
+
+    public void configureSystem(TicketConfiguration config){
+        validateConfiguration(config);
+        this.configuration = config;
+        logEvent("System configured: " + config.toString(), "INFO", "configure system");
+    }
+
+    public void startSystem(){
+        if (isRunning){
+            throw new IllegalAccessException("Error: System is already running.")
         }
-        tickets.put(ticket);
     }
-    public String removeTicket(){
-        return tickets.poll();
-    }
-    public int getTicketCount(){
-        return tickets.size();
-    }
+
 }
 
